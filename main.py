8edf00bec2467ccd6406bb817c464323674d1fd7
Sanidhya Agrawal <sanidhyaagrawal963@gmail.com>
2024-05-08 14:53:25 +0530

    my_project commit

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ import time
 from PIL import Image
 import io
 import base64
+from price_tracker import Revolution
 
 
 app = Flask(__name__)
-talisman = Talisman(app)
 app.secret_key = 'dsgzdfshdfhdgxhghdfhdf'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 login_manager = LoginManager()
@@ -49,32 +49,6 @@ gravatar = Gravatar(app,
 def load_user(user_id):
     return db.get_or_404(Details, user_id)
 
-# Content Security Policy (CSP) Header
-csp = {
-    'default-src': [
-        '\'self\'',
-        'https://code.jquery.com',
-        'https://cdn.jsdelivr.net'
-    ]
-}
-# HTTP Strict Transport Security (HSTS) Header
-hsts = {
-    'max-age': 31536000,
-    'includeSubDomains': True
-}
-
-# Enforce HTTPS and other headers
-talisman.force_https = True
-talisman.force_file_save = True
-talisman.x_xss_protection = True
-talisman.session_cookie_secure = True
-talisman.session_cookie_samesite = 'Lax'
-talisman.frame_options_allow_from = 'https://www.google.com'
- 
-# Add the headers to Talisman
-talisman.content_security_policy = csp
-talisman.strict_transport_security = hsts
-
 
 class MyForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
@@ -90,7 +64,11 @@ class Form(FlaskForm):
     product = StringField('product', validators=[DataRequired()])
     price = StringField('price', validators=[DataRequired()])
     description = StringField('description', validators=[DataRequired()])
-    data_file = MultipleFileField(validators=[FileRequired(), FileAllowed(['png','jpg','jpeg'], 'png and jpg files only')])
+    data_file = FileField(validators=[FileRequired(), FileAllowed(['jpg','jpeg'], 'jpg files only')])
+    company_logo = FileField(validators=[FileRequired(), FileAllowed(['jpg','jpeg'], 'jpg files only')])
+    promotion_video_file = FileField(validators=[FileRequired(), FileAllowed(['mp4','mp3'], 'mp4 and mp3 files only')])
+    category =  StringField('category', validators=[DataRequired()])
+    url =  StringField('url', validators=[])
     enter = SubmitField('enter')
 
 class LoginForm(FlaskForm):
@@ -107,6 +85,7 @@ class Details(UserMixin, db.Model):
     followers = db.Column(db.Integer, nullable=False)
     chips = relationship("Book", back_populates="ate")
     kurkure = relationship("Description", back_populates="eat")
+       
 
 class Description(db.Model):
     __tablename__ = 'product_imf'
@@ -114,35 +93,53 @@ class Description(db.Model):
     product = db.Column(db.String(250), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(250), nullable=False)
+    category = db.Column(db.String(250), nullable=False)
     picture = relationship("Book", back_populates="author")
     eat = relationship("Details", back_populates="kurkure")
     eat_id = db.Column(db.Integer, db.ForeignKey("manager.id"))
+    url = db.Column(db.String(250), nullable=True)
+    option = db.Column(db.String(250), nullable=True)
+
 
 class Book(db.Model):
     __tablename__ = 'images'
     id = db.Column(db.Integer, primary_key=True)
     my_blob = db.Column(db.LargeBinary, nullable=False)
+    product_blob = db.Column(db.LargeBinary, nullable=True)
+    video_blob = db.Column(db.LargeBinary, nullable=True)
     author = relationship("Description", back_populates="picture")
     ate = relationship("Details", back_populates="chips") 
     author_id = db.Column(db.Integer, db.ForeignKey("product_imf.id"))
     ate_id = db.Column(db.Integer, db.ForeignKey("manager.id"))
+
+
+class Follow(db.Model):
+    __tablename__ = 'follows'
+
+    
+    id = db.Column(db.Integer, primary_key=True)
+    follower_id = db.Column(
+        db.Integer,nullable=False)
+ 
+    following_to = db.Column(
+        db.Integer,nullable=False)
 
 with app.app_context():
     db.create_all()    
 
 @app.route('/listed',  methods=['GET', 'POST'])
 def my_route():
-    result = db.session.execute(db.select(Description).order_by(Description.id)).scalars()
-    all_images = db.session.execute(db.select(Book).order_by(Book.id)).scalars()
-    listing = {}
-    for i in result:
-        listing[i.id] = []
-    for j in all_images:
-        corrected_blob = j.my_blob.decode('utf-8')
-        listing[j.author.id].append(corrected_blob)
-    all_books = db.session.execute(db.select(Description).order_by(Description.id)).scalars()     
+    # result = db.session.execute(db.select(Description).order_by(Description.id)).scalars()
+    # all_images = db.session.execute(db.select(Book).order_by(Book.id)).scalars()
+    # listing = {}
+    # for i in result:
+    #     listing[i.id] = []
+    # for j in all_images:
+    #     corrected_blob = base64.b64encode(j.my_blob).decode('utf-8')
+    #     listing[j.author.id].append(corrected_blob)
+    # all_books = db.session.execute(db.select(Description).order_by(Description.id)).scalars()     
 
-    return render_template("product.html", all_books=all_books, listing=listing)
+    return render_template("button.html")
 
 
 @app.route('/forgot_password',  methods=['GET', 'POST'])
@@ -170,8 +167,6 @@ def forgot_password():
             return redirect(url_for('forgot_password'))
     return render_template('otp.html', generate_number=session.get('otpe'))        
 
-
-
 @app.route('/my_listed', methods=['GET', 'POST'])
 def hello_world1():
     result = db.session.execute(db.select(Description).order_by(Description.id)).scalars()
@@ -180,9 +175,9 @@ def hello_world1():
     for i in result:
         change[i.id] = []
     for j in all_images:
-        corrected_blob = j.my_blob.decode('utf-8')
+        corrected_blob = base64.b64encode(j.my_blob).decode('utf-8')
         change[j.author.id].append(corrected_blob)
-    all_books =  db.session.execute(db.select(Description).order_by(Description.id)).scalars()   
+    all_books =  db.session.execute(db.select(Description).order_by(Description.id)).scalars()
     return render_template("admin.html", all_books=all_books, check=current_user, change=change)
 
 @app.route('/',  methods=['GET', 'POST'])
@@ -203,10 +198,43 @@ def hello_name():
    return render_template("Regs.html", regs=regs)
 
 
-@app.route('/profile',  methods=['GET', 'POST'])
-def profile():
-   result = db.session.execute(db.select(Details).order_by(Details.id)).scalars()
-   return render_template('profile.html',result=result)
+@app.route('/profile/<index>',  methods=['GET', 'POST'])
+def profile(index):
+   if request.method == "POST":
+      follow = Follow(follower_backref=current_user.id, following_to_backref=index)
+      db.session.add(follow)
+      db.session.commit()
+   if request.method == "GET":
+    #   thakan = current_user.following_to_list.filter_by(following_to=index).first() 
+      result1 = db.session.execute(db.select(Description).order_by(Description.id)).scalars()
+      all_images = db.session.execute(db.select(Book).order_by(Book.id)).scalars()
+      listing = {}
+      imp_list = []
+      for i in result1:
+        listing[i.id] = []
+      for j in all_images:
+        corrected_blob = base64.b64encode(j.my_blob).decode('utf-8')
+        listing[j.author.id].append(corrected_blob)
+      all_books = db.session.execute(db.select(Description).order_by(Description.id)).scalars()
+      imp_list = [book.category for book in all_books]
+      unique_category = list(set(imp_list))
+      new_list = []
+      for unique in unique_category:
+        book_to_update = db.session.execute(db.select(Description).where(Description.category == unique )).scalars()
+        new_list.append(book_to_update)     
+      result= db.get_or_404(Details, index)
+      followers = db.session.execute(db.select(Follow).where(Follow.follower_id == current_user.id )).scalars()
+      check_list = []
+      for follower in followers: 
+          check_list.append(follower.following_to)    
+      return render_template('profile.html', result=result, all_books=all_books, listing=listing, golang=new_list, current_user=current_user, check_list=check_list)
+    
+@app.route('/unfollow/<index>',  methods=['GET', 'POST'])
+def check(index):
+    user_to_unfollow = current_user.following_to_list.filter_by(
+                following_to=index).first()
+    if user_to_unfollow:
+                db.session.delete(user_to_unfollow)
 
 
 @app.route('/login',  methods=['GET', 'POST'])
@@ -232,39 +260,27 @@ def bhoot():
         
     return render_template("login.html", loginform=loginform, current_user=current_user)
 
-
-
-
 @login_required
 @app.route('/search', methods=['GET'])
 def search():
     billi = Seachform()
     pass
     
-
-
 @login_required
 @app.route('/create_list',  methods=['GET', 'POST'])
 def hello_world():
     series =  Form()
     if series.validate_on_submit():
 
-      list = Description(product=series.product.data, price=series.price.data, description=series.description.data, eat_id=current_user.id)
+      list = Description(product=series.product.data, price=series.price.data, description=series.description.data, eat_id=current_user.id, category=series.category.data, url=series.url.data)
       db.session.add(list)
       db.session.commit()
       
       meow = series.data_file.data
-      for data_file in meow:
-         im = Image.open(data_file)
-         rgb_im = im.convert('RGB')
-         data = io.BytesIO()
-         rgb_im.save(data, "jpeg")
-         encoded_img_data = base64.b64encode(data.getvalue())
-         
-          
-
-         list2 = Book(my_blob=encoded_img_data, author_id=list.id, ate_id=current_user.id)
-         db.session.add(list2)
+      company = series.company_logo.data    
+      promotion = series.promotion_video_file.data    
+      list2 = Book(my_blob=meow.read(), author_id=list.id, ate_id=current_user.id, product_blob=company.read(), video_blob=promotion.read())
+      db.session.add(list2)
           
       db.session.commit()
       
@@ -277,6 +293,40 @@ def hello_world():
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/promotional_site/<index>',  methods=['GET', 'POST'])
+def promotional(index):
+  if "-" in index:  
+    text = index
+    numbers = text.split('-')
+    first_number = int(numbers[0])
+    second_number = int(numbers[1])
+    all_images = db.session.execute(db.select(Book).order_by(Book.id)).scalars()
+    list=[]
+    for images in all_images:
+      if first_number <= images.ate.followers <= second_number:
+        list_dic = {}
+        encoded_img_data = base64.b64encode(images.product_blob).decode('utf-8')
+        base64_video_data = base64.b64encode(images.video_blob).decode('utf-8')
+        list_dic["pics"] = encoded_img_data
+        list_dic["video"] = base64_video_data
+        list_dic["id"] = images.ate.id
+        list.append(list_dic)
+    return render_template("video.html", list=list)
+  else:
+    all_images = db.session.execute(db.select(Book).order_by(Book.id)).scalars()
+    list=[]
+    for images in all_images:
+      if images.ate.followers >= index:
+        list_dic = {}
+        encoded_img_data = base64.b64encode(images.product_blob).decode('utf-8')
+        base64_video_data = base64.b64encode(images.video_blob).decode('utf-8')
+        list_dic["pics"] = encoded_img_data
+        list_dic["video"] = base64_video_data
+        list_dic["id"] = images.ate.id
+        list.append(list_dic)
+    return render_template("video.html", list=list)    
 
 
 @app.route('/delete/<index>',  methods=['GET', 'POST'])
@@ -302,20 +352,56 @@ def edit(index):
             return redirect(url_for('my_route'))
 
     return render_template('pregs.html', series=edit_form)
-        
-# Create an admin-only decorator
-#def admin_only(f):
-    #@wraps(f)
-    #def decorated_function(*args, **kwargs):
-        # If id is not 1 then return abort with 403 error
-        #if current_user.id != 1:
-            #return abort(403)
-        # Otherwise continue with the route function
-        #return f(*args, **kwargs)
 
-    #return decorated_function
+@app.route('/selenium/<object>', methods=['GET','POST'])
+def selenium(object):
+     book_to_update = Follow(follower_id=current_user.id,following_to=object)
+     db.session.add(book_to_update)
+     db.session.commit()
+         
+
+@app.route('/nvidia', methods=['GET','POST'])
+def new_show():
+    updated_book = db.session.execute(db.select(Follow).where(Follow.follower_id == current_user.id )).scalars()
+    updated_list =[]
+    for book in updated_book:
+        new_id = book.following_to
+        object = db.session.query(Description).filter_by(id=new_id).first()
+        print(object.price)
+        updated_list.append(object)    
+    revolution=Revolution("not_required", "not_required")
+    bastard = []
+    dict = {}
+    for updation in updated_list:
+      if int(revolution.method1(updation.url)) >= int(updation.price):
+          dict[updation] = "Yes"
+          bastard.append(dict)
+      else:
+          dict[updation] = "Fuck off"
+          bastard.append(dict)      
+    return dict      
+
+@app.route('/deleting/index', methods=['GET','POST'])
+def true_man(index):
+    updated_book = db.session.execute(db.select(Follow).where(Follow.follower_id == current_user.id )).scalars()
+    for books in updated_book:
+        if books.following_to == index:
+            db.session.delete(books)
+            db.session.commit()
+             
+# Create an admin-only decorator
+# def admin_only(f):
+#     @wraps(f)
+#     def decorated_function(*args, **kwargs):
+#         If id is not 1 then return abort with 403 error
+#         if current_user.id != 1:
+#             return abort(403)
+#         Otherwise continue with the route function
+#         return f(*args, **kwargs)
+
+#     return decorated_function
     
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(debug=True,port=8000)
 
--- price_tracker.py
+++ price_tracker.py
@@ -1,3 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
 class Revolution:
 
     def __init__(self, name, known):
@@ -7,13 +15,25 @@ class Revolution:
         self.speciality = 0
         self.verified = "False"
 
-    def method1(self):
-        if self.followers >= 500:
-            self.speciality += 1
+    def method1(self,url):
+       
+       # Configure Chrome options for headless mode
+       chrome_options = Options()
+       chrome_options.add_argument('--headless')  # Runs Chrome in headless mode
+       chrome_options.add_argument('--disable-gpu')  # Disables GPU hardware acceleration 
+       options = webdriver.ChromeOptions()
+       options.add_experimental_option("detach", True)
+       driver = webdriver.Chrome(options=chrome_options,service=Service(ChromeDriverManager().install()))
+       driver.get(url)
+    #    driver.find_element(By.TAG_NAME, "a").click()
+       price = driver.find_element(By.CLASS_NAME,"a-price-whole")
+       new_price = price.text
+       num_str_without_comma = new_price.replace(',', '') 
+       dot = num_str_without_comma.replace('.', '') 
+
+       driver.quit()
+       return dot
             
     def method2(self):
         if self.followers >= 1000:
             self.speciality = True
-
-
-
